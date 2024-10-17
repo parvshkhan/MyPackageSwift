8a610992e42817cb573f76531e03427bc4dfbbf3
parveshkhan <Parvesh@gribb.tech>
2024-10-17 15:28:07 +0530

    Initial Commit

---
+++ Sources/MyPackageSwift/HomeScreenVC.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import UIKit
+public class HomeScreenVC: UIViewController{
+    
+    public static let storyboardVC = UIStoryboard(name: "main", bundle: Bundle.module).instantiateInitialViewController()!
+    
+   public override func viewDidLoad() {
+   }
+}
